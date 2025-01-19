b34463612df293848263b8731f667fee072eda08
Hope <starrvansittert@gmail.com>
2025-01-19 16:08:29 +0200

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import django
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Hope'
+copyright = '2025, Hope'
+author = 'Hope'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    ]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hyperion.settings'
+django.setup()
---
+++ docs/modules.rst
@@ -0,0 +1,10 @@
+L2T22 - Django - Poll App and Authentication
+============================================
+
+.. toctree::
+   :maxdepth: 4
+
+   hyperion
+   manage
+   polls
+   user_auth
